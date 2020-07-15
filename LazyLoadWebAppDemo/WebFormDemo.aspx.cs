9755864240471ea1bb13f18554aab4ef235a0a66
Fatih Ceylan <fceylan001@gmail.com>
2020-07-15 15:33:37 +0300

    WebForm added

---
+++ LazyLoadWebAppDemo/WebFormDemo.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LazyLoadWebAppDemo
+{
+    public partial class WebFormDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (WebAppSampleDataContext dbContext = new WebAppSampleDataContext())
+            {
+                // Log the sql query
+                dbContext.Log = Response.Output;
+
+                GridView1.DataSource = dbContext.Departments;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
