e8723783f65b4b60ab038cc911485fc5f06afe1d
Fatih Ceylan <fceylan001@gmail.com>
2020-07-15 14:19:08 +0300

    Console demo added

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SampleDataContext dbContext = new SampleDataContext())
+            {
+                dbContext.Log = Console.Out;
+                foreach (Department department in dbContext.Departments)
+                {
+                    Console.WriteLine(department.Name);
+                    foreach (Employee employee in department.Employees)
+                    {
+                        Console.WriteLine("\t" + employee.FirstName + " " + employee.LastName);
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
