3b601fc722b2521e058497f4a5321dc1af342b43
Fatih-Ceylan <fceylan001@gmail.com>
2020-07-17 23:48:57 +0300

    Concurrency

--- ConcurrencyDemo/Concurrency.designer.cs
+++ ConcurrencyDemo/Concurrency.designer.cs
@@ -22,6 +22,7 @@ namespace ConcurrencyDemo
 	using System;
 	
 	
+	[global::System.Data.Linq.Mapping.DatabaseAttribute(Name="Sample")]
 	public partial class ConcurrencyDataContext : System.Data.Linq.DataContext
 	{
 		
@@ -29,7 +30,16 @@ namespace ConcurrencyDemo
 		
     #region Extensibility Method Definitions
     partial void OnCreated();
+    partial void InsertAccount(Account instance);
+    partial void UpdateAccount(Account instance);
+    partial void DeleteAccount(Account instance);
     #endregion
+		
+		public ConcurrencyDataContext() : 
+				base(global::System.Configuration.ConfigurationManager.ConnectionStrings["SampleConnectionString"].ConnectionString, mappingSource)
+		{
+			OnCreated();
+		}
 		
 		public ConcurrencyDataContext(string connection) : 
 				base(connection, mappingSource)
@@ -53,6 +63,124 @@ namespace ConcurrencyDemo
 				base(connection, mappingSource)
 		{
 			OnCreated();
+		}
+		
+		public System.Data.Linq.Table<Account> Accounts
+		{
+			get
+			{
+				return this.GetTable<Account>();
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.Accounts")]
+	public partial class Account : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _AccountNumber;
+		
+		private string _AccountName;
+		
+		private System.Nullable<int> _AccountBalance;
+		
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnAccountNumberChanging(int value);
+    partial void OnAccountNumberChanged();
+    partial void OnAccountNameChanging(string value);
+    partial void OnAccountNameChanged();
+    partial void OnAccountBalanceChanging(System.Nullable<int> value);
+    partial void OnAccountBalanceChanged();
+    #endregion
+		
+		public Account()
+		{
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_AccountNumber", DbType="Int NOT NULL", IsPrimaryKey=true)]
+		public int AccountNumber
+		{
+			get
+			{
+				return this._AccountNumber;
+			}
+			set
+			{
+				if ((this._AccountNumber != value))
+				{
+					this.OnAccountNumberChanging(value);
+					this.SendPropertyChanging();
+					this._AccountNumber = value;
+					this.SendPropertyChanged("AccountNumber");
+					this.OnAccountNumberChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_AccountName", DbType="NVarChar(50)")]
+		public string AccountName
+		{
+			get
+			{
+				return this._AccountName;
+			}
+			set
+			{
+				if ((this._AccountName != value))
+				{
+					this.OnAccountNameChanging(value);
+					this.SendPropertyChanging();
+					this._AccountName = value;
+					this.SendPropertyChanged("AccountName");
+					this.OnAccountNameChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_AccountBalance", DbType="Int")]
+		public System.Nullable<int> AccountBalance
+		{
+			get
+			{
+				return this._AccountBalance;
+			}
+			set
+			{
+				if ((this._AccountBalance != value))
+				{
+					this.OnAccountBalanceChanging(value);
+					this.SendPropertyChanging();
+					this._AccountBalance = value;
+					this.SendPropertyChanged("AccountBalance");
+					this.OnAccountBalanceChanged();
+				}
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
 		}
 	}
 }
---
+++ ConcurrencyDemo/WebForm1.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ConcurrencyDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-family: Arial">
+            <table border="1">
+                <tr>
+                    <td>
+                        <b>Account Number</b>
+                    </td>
+                    <td>
+                        <asp:Label ID="lblAccountNumber" runat="server"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Account Name</b>
+                    </td>
+                    <td>
+                        <asp:Label ID="lblAccountName" runat="server"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Account Balance</b>
+                    </td>
+                    <td>
+                        <asp:Label ID="lblAccountBalance" runat="server"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:Button ID="btnDeposit" runat="server" Text="Deposit $500"
+                OnClick="btnDeposit_Click" />
+        </div>
+    </form>
+</body>
+</html>
