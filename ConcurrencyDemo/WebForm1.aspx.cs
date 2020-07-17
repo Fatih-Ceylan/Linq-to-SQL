6eb75cd3b5de3ac318ae78efe7dda908055b75c8
Fatih-Ceylan <fceylan001@gmail.com>
2020-07-17 23:56:31 +0300

    Update WebForm1.aspx.cs

--- ConcurrencyDemo/WebForm1.aspx.cs
+++ ConcurrencyDemo/WebForm1.aspx.cs
@@ -11,12 +11,34 @@ namespace ConcurrencyDemo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                GetAccountsData();
+            }
         }
 
         protected void btnDeposit_Click(object sender, EventArgs e)
         {
+            using (ConcurrencyDataContext db = new ConcurrencyDataContext())
+            {
+                Account account = db.Accounts.FirstOrDefault(x => x.AccountNumber == 1);
 
+                account.AccountBalance = account.AccountBalance + 500;
+                db.SubmitChanges();
+            }
+
+        }
+
+        private void GetAccountsData()
+        {
+            using (ConcurrencyDataContext db = new ConcurrencyDataContext())
+            {
+                Account account = db.Accounts.FirstOrDefault(x => x.AccountNumber == 1);
+
+                lblAccountNumber.Text = account.AccountNumber.ToString();
+                lblAccountName.Text = account.AccountNumber.ToString();
+                lblAccountBalance.Text = account.AccountBalance.ToString();
+            }
         }
     }
 }
