60cb20f6454211e74fc1b6c546205092b6616ac0
Fatih-Ceylan <fceylan001@gmail.com>
2020-07-18 22:33:03 +0300

    Update WebForm1.aspx.cs

--- ConcurrencyDemo/WebForm1.aspx.cs
+++ ConcurrencyDemo/WebForm1.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Linq;
 using System.Linq;
 
 namespace ConcurrencyDemo
@@ -17,12 +18,20 @@ namespace ConcurrencyDemo
         {
             using (ConcurrencyDataContext db = new ConcurrencyDataContext())
             {
-                Account account = db.Accounts.FirstOrDefault(x => x.AccountNumber == 1);
+                try
+                {
+                    Account account = db.Accounts.FirstOrDefault(x => x.AccountNumber == 1);
 
-                account.AccountBalance = account.AccountBalance + 500;
-                db.SubmitChanges();
+                    account.AccountBalance = account.AccountBalance + 500;
+                    db.SubmitChanges();
 
-                GetAccountsData();
+                    GetAccountsData();
+                }
+                catch (ChangeConflictException)
+                {
+
+                    db.ChangeConflicts.ResolveAll(RefreshMode.KeepCurrentValues);
+                }
             }
 
         }
